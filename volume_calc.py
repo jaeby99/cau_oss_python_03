bb9d108e1f3da3d7cd889a3c3e974fec1a4c1f92
jaeby99 <qo0102@naver.com>
2023-03-21 15:34:26 +0900

    2주차

--- volume_calc.py
+++ volume_calc.py
@@ -2,6 +2,15 @@ width = float(input("가로: "))
 length = float(input("세로: "))
 height = float(input("높이: "))
 
-volume = width * length * height
+volume = width + length + height
 
-print("박스의 부피는",volume,"입니다.")
+if volume <= 80:
+    fee = 5000
+elif volume <= 100:
+    fee = 8000
+elif volume <= 120:
+    fee = 10000
+elif volume <= 160:
+    fee = 13000
+
+print("해당 택배의 배송요금은", fee, "입니다")
