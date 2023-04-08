9757179008ae666decf3ccb5e683151edd5d9b70
jaeby99 <qo0102@naver.com>
2023-04-08 22:30:27 +0900

    calculator 수정

--- simple_calculator.py
+++ simple_calculator.py
@@ -23,22 +23,33 @@ def arithmetic_ops(op):
 
 # +와 -는 함수로 정의한다
 # [fill this area] +와 -함수 정의
-#
+
+def add(num1, num2):
+    return num1 + num2
+
+def sub(num1, num2):
+    return num1 - num2
 
 while True:
     op = input("input operation:")
     if op == "end":
-        [fill this blank]   # "반복을 종료"
+        break   # "반복을 종료"
     elif op == "+":
-        [fill this blank] = arithmetic_ops([fill this blank]) # 정의된 함수 사용
+        num1, num2, ret = arithmetic_ops(add) # 정의된 함수 사용
     elif op == "*":
-        [fill this blank] = arithmetic_ops([fill this blank]) # 익명함수(lambda) 사용
+        num1, num2, ret = arithmetic_ops(lambda x, y: x*y) # 익명함수(lambda) 사용
+    elif op == "-":
+        num1, num2, ret = arithmetic_ops(sub)
+    elif op == "/":
+        num1, num2, ret = arithmetic_ops(lambda x, y: x/y)
+    elif op == "%":
+        num1, num2, ret = arithmetic_ops(lambda x, y: x%y)
     #
     # [fill this area] 위의 코드를 참고하여 -, /, %에 대한 내용 구현
     #
     else:
         print("Invalid operation")
-        [fill this blank] # Invalid operation이므로 연산결과를 출력하지 않고 "넘어간다".
+        continue # Invalid operation이므로 연산결과를 출력하지 않고 "넘어간다".
     print(f"{num1}{op}{num2} = {ret}")  # 연산 결과를 출력
 
 print("Exit program")
